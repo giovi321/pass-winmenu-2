8a8a8ade85f29f6311480fe25fbcdb4ea242670f
giovi321 <giovi321>
2026-06-28 15:23:26 +0200

    Document special-characters block in default config
    giovi321

--- pass-winmenu-tests/Configuration/ConfigFileTests.cs
+++ pass-winmenu-tests/Configuration/ConfigFileTests.cs
@@ -1,4 +1,6 @@
 using System.IO;
+using PassWinmenu.Configuration;
+using Shouldly;
 using Xunit;
 using YamlDotNet.Serialization;
 
@@ -14,6 +16,17 @@ namespace PassWinmenuTests.Configuration
 
 			// Will throw an exception if the file does not contain valid YAML
 			des.Deserialize(File.OpenText(@"..\..\..\..\pass-winmenu\embedded\default-config.yaml"));
+		}
+
+		[Fact]
+		public void ConfigFile_SpecialCharactersBlock_Deserialises()
+		{
+			using var reader = File.OpenText(@"..\..\..\..\pass-winmenu\embedded\default-config.yaml");
+			var config = ConfigurationDeserialiser.Deserialise<Config>(reader);
+
+			var sc = config.PasswordStore.PasswordGeneration.SpecialCharacters;
+			sc.Characters.ShouldBe("!@#$%^&*");
+			sc.Placement.ShouldBe(SpecialCharacterPlacement.End);
 		}
 	}
 }
