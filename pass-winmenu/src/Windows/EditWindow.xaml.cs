dfe6ca01d1fb5e0a54779a6bfe4a801367cec304
giovi321 <giovi321>
2026-06-28 15:23:26 +0200

    Host PasswordGeneratorControl in EditWindow
    giovi321

--- pass-winmenu/src/Windows/EditWindow.xaml.cs
+++ pass-winmenu/src/Windows/EditWindow.xaml.cs
@@ -1,18 +1,15 @@
 using System;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using PassWinmenu.Configuration;
-using PassWinmenu.PasswordGeneration;
 
 #nullable enable
 namespace PassWinmenu.Windows
 {
 	internal sealed partial class EditWindow
 	{
-		private readonly PasswordGenerator passwordGenerator;
 		private readonly string originalContent;
 
 		public EditWindow(string path, string content, PasswordGenerationConfig options)
@@ -20,8 +17,7 @@ namespace PassWinmenu.Windows
 			WindowStartupLocation = WindowStartupLocation.CenterScreen;
 			InitializeComponent();
 
-			passwordGenerator = new PasswordGenerator(options);
-			CreateCheckboxes();
+			Generator.Initialize(options);
 
 			Title = $"Editing '{path}'";
 
@@ -31,48 +27,12 @@ namespace PassWinmenu.Windows
 			PasswordContent.Focus();
 		}
 
-		private void CreateCheckboxes()
-		{
-			var colCount = 3;
-			var index = 0;
-			foreach (var charGroup in passwordGenerator.Options.CharacterGroups)
-			{
-				var x = index % colCount;
-				var y = index / colCount;
-
-				var cbx = new CheckBox
-				{
-					Name = charGroup.Name,
-					Content = charGroup.Name,
-					Margin = new Thickness(x * 100, y * 20, 0, 0),
-					HorizontalAlignment = HorizontalAlignment.Left,
-					VerticalAlignment = VerticalAlignment.Top,
-					IsChecked = charGroup.Enabled,
-				};
-				cbx.Unchecked += HandleCheckedChanged;
-				cbx.Checked += HandleCheckedChanged;
-				CharacterGroups.Children.Add(cbx);
-
-				index++;
-			}
-		}
-
-		private void RegeneratePassword()
-		{
-			Password.Text = passwordGenerator.GeneratePassword();
-			Password.CaretIndex = Password.Text?.Length ?? 0;
-		}
-
-		private void Btn_Generate_Click(object sender, RoutedEventArgs e)
-		{
-			Password.Text = passwordGenerator.GeneratePassword();
-		}
-
 		private void Btn_Replace_Click(object sender, RoutedEventArgs e)
 		{
 			var content = PasswordContent.Text.Replace(Environment.NewLine, "\n");
 			var index = content.IndexOf('\n');
-			PasswordContent.Text = index == -1 ? Password.Text : Password.Text + content.Remove(0, index);
+			var password = Generator.GeneratedPassword;
+			PasswordContent.Text = index == -1 ? password : password + content.Remove(0, index);
 		}
 
 		private void Btn_OK_Click(object sender, RoutedEventArgs e)
@@ -94,14 +54,6 @@ namespace PassWinmenu.Windows
 				DialogResult = false;
 				Close();
 			}
-		}
-
-		private void HandleCheckedChanged(object sender, RoutedEventArgs e)
-		{
-			var checkbox = (CheckBox)sender;
-			passwordGenerator.Options.CharacterGroups.First(c => c.Name == checkbox.Name).Enabled = checkbox.IsChecked ?? false;
-
-			RegeneratePassword();
 		}
 
 		private void HandlePasswordContentFocus(object sender, RoutedEventArgs e)
